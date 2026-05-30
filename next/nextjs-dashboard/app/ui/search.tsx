d3834918380f2de85e8722899ff8e29d8d054bf4
davidovale <dovale.sis@gmail.com>
2026-05-30 20:31:24 -0300

    test

--- next/nextjs-dashboard/app/ui/search.tsx
+++ next/nextjs-dashboard/app/ui/search.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { useDebouncedCallback } from 'use-debounce';
+
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
