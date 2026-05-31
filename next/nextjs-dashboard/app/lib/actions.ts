3d32967a0c6fc3538549c9d93545674e611ed0cd
davidovale <dovale.sis@gmail.com>
2026-05-31 00:22:33 -0300

    test

--- next/nextjs-dashboard/app/lib/actions.ts
+++ next/nextjs-dashboard/app/lib/actions.ts
@@ -109,17 +109,17 @@ export async function updateInvoice(
   redirect('/dashboard/invoices');
 }
 
-export async function deleteInvoice(id: string, formData: FormData) {
-  try {
+export async function deleteInvoice(id: string) {
+  //try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
-    return { message: "Deleted Invoice." };
+    /*return { message: "Deleted Invoice." };
   } catch (error) {
     console.error(error);
     return {
       message: "Database Error: Failed to Delete Invoice.",
     };
-  }
+  }*/
 }
 
 export async function authenticate(
